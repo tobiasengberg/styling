abf86af409b3500c2e178bec14ec4314b9584ce8
tobiasengberg <tobias.engberg@exilic.se>
2025-10-08 23:12:07 +0200

    Add navigation

---
+++ ui-libraries/bootstrap/react/bootstrap-styling/src/layout/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar bg="exilic" fixed="top" expand="lg" >
+                <Container>
+                    <Navbar.Brand href="#home">Place Software</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                        <Nav.Link href="#home">Hem</Nav.Link>
+                        <Nav.Link href="#link">Kontakt</Nav.Link>
+                        <NavDropdown title="Teman" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Index</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                            Semantik
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Graf</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                            </NavDropdown.Item>
+                    </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Navigation;
