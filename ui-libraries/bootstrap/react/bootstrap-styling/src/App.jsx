bc28e2de2479c76444decd34fe3543229d6e58d7
tobiasengberg <tobias.engberg@exilic.se>
2025-11-18 23:45:12 +0100

    Bootstrap form

--- ui-libraries/bootstrap/react/bootstrap-styling/src/App.jsx
+++ ui-libraries/bootstrap/react/bootstrap-styling/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
       <>
           <Navigation />
-          <div className="container-fluid">
+          <div className="main container-fluid">
             <Outlet/>
           </div>
       </>
--- ui-libraries/bootstrap/react/bootstrap-styling/src/layout/CustomerForm.jsx
+++ ui-libraries/bootstrap/react/bootstrap-styling/src/layout/CustomerForm.jsx
@@ -8,25 +8,47 @@ import Col from 'react-bootstrap/Col';
 const CustomerForm = () => {
     return (
         <Container>
-            <Form>
-                <Form.Group as={Row} className="mb-3" controlId="customerName">
-                    <Form.Label column sm="2">Namn:</Form.Label>
-                    <Col sm="10">
-                    <Form.Control type="text" placeholder="Namn" />
-                    </Col>
+            <Form className="border p-3">
+                <Form.Group as={Row} className="mb-3" controlId="customerId1">
+                    <Form.Group as={Col} className="mb-3" controlId="customerName1">
+                        <Form.Label  column sm="2">Namn:</Form.Label>
+                        <Col sm="10">
+                        <Form.Control type="text" placeholder="Namn" />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3" controlId="customerAddress1">
+                        <Form.Label column sm="2">Adress:</Form.Label>
+                        <Col sm="10">
+                        <Form.Control type="text" placeholder="Adress" />
+                        </Col>
+                    </Form.Group>
+
                 </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="customerAddress">
-                    <Form.Label column sm="2">Adress:</Form.Label>
-                    <Col sm="10">
-                    <Form.Control type="text" placeholder="Adress" />
-                    </Col>
+                <Form.Group as={Row} className="mb-3" controlId="customerId2">
+                    <Form.Group as={Col} className="mb-3" controlId="customerName">
+                        <Form.Label  column sm="2">Namn:</Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="text" placeholder="Namn" />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3" controlId="customerAddress">
+                        <Form.Label column sm="2">Adress:</Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="text" placeholder="Adress" />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="customerHome">
+                        <Form.Group as={Col} className="mb-3">
+                        <Form.Check type="checkbox" label="Hemadress" />
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3">
+                        <Button className="row align-self-end" variant="primary" type="submit">
+                            Skicka
+                        </Button>
+                        </Form.Group>
+                    </Form.Group>
+
                 </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="customerHome">
-                    <Form.Check type="checkbox" label="Hemadress" />
-                </Form.Group>
-                <Button  as={Row} className="row align-self-end" variant="primary" type="submit">
-                    Skicka
-                </Button>
             </Form>
         </Container>
     );
