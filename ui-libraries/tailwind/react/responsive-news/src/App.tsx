3b2f4c07fc8345515c983c17b913dd944b1bb1f3
tobiasengberg <tobias.engberg@exilic.se>
2026-01-13 23:30:50 +0100

    Grid type

--- ui-libraries/tailwind/react/responsive-news/src/App.tsx
+++ ui-libraries/tailwind/react/responsive-news/src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
-import Box from "./components/Box.tsx";
 
 function App() {
 
-    const boxes = [8, 2, 4, 6, 4, 6, 10, 8, 4, 6, 2, 2, 4]
   return (
-    <div id="site-container" className="mx-auto grid grid-cols-16 gap-4 grid-flow-dense w-xl md:w-3xl lg:w-5xl xl:w-7xl">
-        {boxes.map((box) => (
-            <Box span={box} />
-        ))}
-        <div>
-
+    <div id="site-container" className="mx-auto grid grid-cols-16 grid-rows-10 gap-4 grid-flow-dense w-xl md:w-3xl lg:w-5xl xl:w-7xl">
+        <div className="col-start-4 col-end-8 row-start-2 row-end-3 h-10">
+            <p className="text-[300px] m-0 p-0 font-bold leading-none tracking-tight text-gray-200">Drop</p>
         </div>
-      <h1 className="text-3xl lg:text-amber-600">Responsive news site, using Tailwind</h1>
+        <div className="col-start-5 col-end-12 row-start-6 row-end-8 h-10">
+            <p className="text-3xl m-0 p-0 font-bold leading-none tracking-tight text-red-300">Framing the issue as a possibility</p>
+        </div>
+        <div className="col-start-8 col-end-12 row-start-4 row-end-8 h-10">
+            <p className="text-6xl m-0 p-0 font-bold leading-none tracking-tight text-blue-300">all pretenses</p>
+        </div>
     </div>
   )
 }
