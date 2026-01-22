2564a2b67d02ffc92564739a21c9c0f095537486
tobiasengberg <tobias.engberg@exilic.se>
2026-01-22 23:37:06 +0100

    Add graphical composition 3

--- ui-libraries/tailwind/react/responsive-news/src/App.tsx
+++ ui-libraries/tailwind/react/responsive-news/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Composition1 from "./compositions/Composition1.tsx";
 import Composition2 from "./compositions/Composition2.tsx";
+import Composition3 from "./compositions/Composition3.tsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div id="site-container" className="mx-auto grid grid-cols-16 grid-rows-10 gap-4 grid-flow-dense w-xl md:w-3xl lg:w-5xl xl:w-7xl">
         <Composition1 />
         <Composition2 />
+        <Composition3 />
     </div>
   )
 }
---
+++ ui-libraries/tailwind/react/responsive-news/src/compositions/Composition3.tsx
@@ -0,0 +1,15 @@
+
+const Composition3 = () => {
+    return (
+        <div className="col-start-1 col-end-4 row-start-1 row-end-15 relative">
+            <p className="text-amber-800 text-9xl font-bold -rotate-12 absolute -top-3 left-8">C</p>
+            <p className="text-amber-700 text-9xl font-bold rotate-18 absolute top-12 left-23">o</p>
+            <p className="text-amber-400 text-9xl font-bold rotate-4 absolute top-42 left-0">o</p>
+            <p className="text-amber-400 text-9xl font-bold -rotate-34 absolute top-92 left-34">p</p>
+            <p className="text-amber-400 text-9xl font-bold -rotate-16 absolute top-114 left-14">e</p>
+            <p className="text-amber-300 text-9xl font-bold rotate-22 absolute top-123 left-34">r</p>
+        </div>
+    );
+};
+
+export default Composition3;
